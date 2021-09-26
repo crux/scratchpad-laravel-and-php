86e302c945aeb991cfa1b7a53f396af0fae2cf1b
dirk lüsebrink <dirk.luesebrink@gmail.com>
2021-09-26 14:34:35 +0200

    laravel/breeze setup, initial
    https://laravel.com/docs/8.x/starter-kits
    
    ```
    $ composer require laravel/breeze --dev
    $ php artisan breeze:install
    $ npm install
    $ npm run dev
    $ php artisan migrate
    ```

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
-    return view('hi'); //welcome');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
